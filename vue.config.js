1a5e09dbbb3506d0f6f617351139a3944f8ca5f1
IreneChou05 <48037854+IreneChou05@users.noreply.github.com>
2021-06-17 11:05:46 +0800

    2021.6.17

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
         },
       }
     },
-  publicPath: process.env.NODE_ENV === 'development'
+  publicPath: process.env.NODE_ENV === 'production'
     ? '/destributed_system/'
     : '/'
   // presets: [
